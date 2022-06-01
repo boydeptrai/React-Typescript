723d0555999a5f4ddabf88fbc75a80ce4ac92e3c
Tuan Tran <langthamht047@gmail.com>
2022-05-31 20:51:31 -0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Form from './components/Form'
+import List from './components/List'
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export interface IState {
+  people: {
+    name: string,
+    age: number,
+    bio: string
+  }[]
+
 }
-
-export default App;
+export default function App() {
+  const [people,setPeople] = useState<IState["people"]>([{name:"Tuan",age:30,bio:"developer"}])
+  return (
+    <div className="container">
+      <Form people={people} setPeople={setPeople}/>
+      <List people={people}/>
+    </div>
+  )
+}
